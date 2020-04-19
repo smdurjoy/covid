f18c10667807849f8725f7d5fa005e702cfe13d1
Saqlain Mustaq Durjoy <49731654+smdurjoy@users.noreply.github.com>
2020-04-19 19:03:48 +0000

    Update BdDetails.js

--- src/components/BdDetails/BdDetails.js
+++ src/components/BdDetails/BdDetails.js
@@ -88,8 +88,8 @@ export class BdDetails extends Component {
                                 <th scope="col">TotalDeaths</th>
                             </tr>  
                             <tr>
-                                <td scope="col">306</td>
-                                <td scope="col">9</td>
+                                <td scope="col">312</td>
+                                <td scope="col">7</td>
                             </tr>
                             <tr>
                             <Link  className="bdlink" to="/BdAllInfo"><span class="thin">More</span><span class="thick"> Info</span></Link>
